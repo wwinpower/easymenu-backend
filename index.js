6506c2927d2d5ce45cd48dfd80eafb22afcda7a8
Vladimir <vladimir.abrahamyan96@gmail.com>
2024-03-31 20:59:36 +0400

    EasyMenu

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const {connect} = require("mongoose");
 
 const index = express();
@@ -10,8 +9,7 @@ index.use(bodyParser.urlencoded({
     extended: true
 }))
 
-index.use(bodyParser.json())
-// index.use(morgan('dev'));
+index.use(bodyParser.json());
 
 require('./app/routes/product.route.js')(index);
 
@@ -20,17 +18,17 @@ index.get("/", (req, res) => res.send("Express on Vercel"));
 const PORT = process.env.PORT || 8080;
 
 async function start() {
-    await connect(`${process.env.MONGO_URL}`, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
-        console.log("Успешно подключился к базе данных");
+    // await connect(`${process.env.MONGO_URL}`, { useNewUrlParser: true }).then(() => {
+    //     console.log("Успешно подключился к базе данных");
 
         index.listen(PORT, () => {
             console.log("Сервер прослушивает порт 3000");
         });
 
-    }).catch(err => {
-        console.log('Не удалось подключиться к базе данных.', err);
-        process.exit();
-    });
+    // }).catch(err => {
+    //     console.log('Не удалось подключиться к базе данных.', err);
+    //     process.exit();
+    // });
 }
 
 start();
